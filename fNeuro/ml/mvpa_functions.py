43e5cf8da8fec89ea0dafc848466a6ba8383c591
Daniel Halls <p3dh0001@gmail.com>
2023-10-06 15:23:44 +0100

    moved pickle functions to utils.pickling

--- fNeuro/ml/mvpa_functions.py
+++ fNeuro/ml/mvpa_functions.py
@@ -1,4 +1,3 @@
-import pickle
 from decouple import config
 import re
 import os
@@ -9,52 +8,6 @@ from nilearn.maskers import NiftiSpheresMasker
 import nilearn
 import numpy as np
 from fNeuro.second_level.second_level_functions import bayesian_cluster_info
-
-def save_pickle(name: str, object_to_pickle: object) -> None:
-
-    '''
-    Function to save an object as pickle file.
-   
-    Parameters
-    ----------
-    name: str 
-        str of name of file. Include full path
-    object_to_pickle: object 
-        object to save as pickle file
-    
-    Returns
-    -------
-    None
-    '''
-
-    with open(f'{name}.pickle', 'wb') as handle:
-        pickle.dump(object_to_pickle, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-
-def load_pickle(name_of_pickle_object: str) -> object:
-
-    '''
-    Function to load pickle object.
-
-    Parameters
-    ----------
-    name_of_pickle_object: str 
-        name of object to be loaded. 
-        Doesn't need extension however needs full path
-
-    Returns
-    -------
-    unpickled obect
-    '''
-
-
-    try: 
-        with open(f'{name_of_pickle_object}.pickle', 'rb') as handle:
-            return pickle.load(handle)
-    except Exception:
-        print('Unable to load pickle object')
-
-
 
 def get_ados_df(group: str):
     '''
