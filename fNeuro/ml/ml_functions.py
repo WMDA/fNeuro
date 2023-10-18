503946011de919a88cdac5182055159dfa4e1c3f
Daniel Halls <p3dh0001@gmail.com>
2023-10-18 16:12:31 +0100

    added estimate regression model to fNeuro

--- fNeuro/ml/ml_functions.py
+++ fNeuro/ml/ml_functions.py
@@ -28,3 +28,35 @@ def estimate_model(model, X: np.array, y: np.array) -> dict:
         'Accuracy': cross_val_score(model, X, y, cv=cv, scoring='accuracy', n_jobs=2),
         'ROC_AUC': cross_val_score(model, X, y, cv=cv, scoring='roc_auc', n_jobs=2),
     }
+
+def estimate_regression_model(model: object, X: np.array, y: np.array) -> dict:
+    
+    '''
+    Function to estimate regression models with cross validation
+
+    Parameters
+    ----------
+    model: object 
+        instance of sckit learn model
+    
+    X: np.array
+        array of X values
+
+    y: np.array of y values
+        array of y values
+
+    Returns
+    -------
+    dict: dictionary object
+        dict of r2, MAE and MSE values
+    '''
+
+    cv = StratifiedShuffleSplit(n_splits=10, random_state=42, test_size=0.2, train_size=0.8)
+    try: 
+        return {
+        'r2': cross_val_score(model, X, y, cv=cv, scoring='r2', n_jobs=3),
+        'MAE': cross_val_score(model, X, y, cv=cv, scoring='neg_mean_absolute_error', n_jobs=3),
+        'MSE': cross_val_score(model, X, y, cv=cv, scoring='neg_mean_squared_error', n_jobs=3),
+    }
+    except Exception as e:
+        print(e)
